25ef7d64690a7fbd05b03b7f1b5cf64ebbf6fd5d
George Vicarey <g.vicarey@hotmail.co.uk>
2014-11-03 08:21:19 +0000

    Attempting to render opengl triangle

--- main.cpp
+++ main.cpp
@@ -9,75 +9,100 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 
+const GLchar *vertexSource = "#version 150\n"
+		"in vec2 position;"
+		"void main()"
+		"{"
+		" gl_Position = vec4(position, 0.0, 1.0);"
+		"}";
+
+const GLchar *fragmentSource = "#version 150\n"
+		"out vec4 outColour;"
+		"void main()"
+		"{"
+		" outColour = vec4(1.0, 1.0, 1.0, 1.0);"
+		"}";
+
 int main(int argc, char *argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
 
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+			SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
-	SDL_Window* window = SDL_CreateWindow("OpenGL Test", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+	SDL_Window* window = SDL_CreateWindow("OpenGL Test", 100, 100, 800, 600,
+			SDL_WINDOW_OPENGL);
 
 	SDL_GLContext context = SDL_GL_CreateContext(window);
 
+	// initialise GLEW
 	glewExperimental = GL_TRUE;
 	glewInit();
 
-	SDL_Event windowEvent;
-	while (true) {
-		if (SDL_PollEvent(&windowEvent)) {
-			if (windowEvent.type == SDL_QUIT) break;
-		}
+	//create Vertex array object
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
-		SDL_GL_SwapWindow(window);
-	}
-
-	float vertices[] = {
-			0.0f, 0.5f,
-			0.5f, -0.5f,
-			-0.5f, -0.5f
-	};
-
+	//create vertex buffer object and copy data to it
 	GLuint vertexBuffer;
 	glGenBuffers(1, &vertexBuffer);
+
+	float vertices[] = { 0.0f, 0.5f, 0.5f, -0.5f, -0.5f, -0.5f };
 
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	static const GLchar *vertexSource =
-			"#version 150\n"
-			"in vec2 position;"
-			"void main()"
-			"{"
-				" gl_Position = vec4(position, 0.0, 1.0);"
-			"}\0";
-
+	// create and compile vertex shader
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexSource, NULL);
 	glCompileShader(vertexShader);
 
-	static const GLchar *fragmentSource =
-			"#version 150\n"
-			"out vec4 outColour;"
-			"void main()"
-			"{"
-				" outColour = vec4(1.0, 1.0, 1.0, 1.0);"
-			"}\0";
 
+	// create and compile fragment shader
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
 	glCompileShader(fragmentShader);
 
+	// link the vertex and fragment shader into a shader program
 	GLuint shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
-
 	glBindFragDataLocation(shaderProgram, 0, "outColour");
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
 
-	// up to Making the link between vertex data and attributes
-	//https://open.gl/drawing
+	// specify the layout of the vertex data
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(posAttrib);
 
-	printf("%u\n", vertexBuffer);
+	// handle events
+	SDL_Event e;
+
+	while (true) {
+		if (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT)
+				break;
+		}
+		// clear screen to black
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// draw triangle from the 3 vertices
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		SDL_GL_SwapWindow(window);
+	}
+
+	glDeleteProgram(shaderProgram);
+	glDeleteShader(fragmentShader);
+	glDeleteShader(vertexShader);
+
+	glDeleteBuffers(1, &vertexBuffer);
+
+	glDeleteVertexArrays(1, &vao);
 
 	SDL_GL_DeleteContext(context);
 	SDL_Quit();

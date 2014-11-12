ec8df953501e7d150750297ae7fc99d951cfb1a6
George Vicarey <G.Vicarey@hotmail.co.uk>
2014-11-12 20:14:22 +0000

    Removed Debug directory

--- Debug/src/shader.d
+++
@@ -1,7 +0,0 @@
-src/shader.d: ../src/shader.cpp \
- /home/gv30/H_Drive/Simple\ OpenGL\ Image\ Library/src/SOIL.h \
- ../src/shader.h
-
-/home/gv30/H_Drive/Simple\ OpenGL\ Image\ Library/src/SOIL.h:
-
-../src/shader.h:

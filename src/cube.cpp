846956386680e7e1e914480e0401eddab16f85b0
George Vicarey <G.Vicarey@hotmail.co.uk>
2014-11-17 11:42:20 +0000

    linux bug

--- src/cube.cpp
+++ src/cube.cpp
@@ -94,9 +94,9 @@ void Cube::createCube() {
 	glGenBuffers(1, &ebo);
 
 	GLuint elements[] = {
-			0, 1, 2, 2, 3, 0,
-			4, 5, 6, 6, 7, 4,
-			8, 9, 10, 10, 11, 8,
+			0,  1,  2,  2,  3,  0,
+			4,  5,  6,  6,  7,  4,
+			8,  9,  10, 10, 11, 8,
 			12, 13, 14, 14, 15, 12,
 			16, 17, 18, 18, 19, 16,
 			20, 21, 22, 22, 23, 20
@@ -146,6 +146,7 @@ void Cube::update() {
 	    (float)clock() / (float)CLOCKS_PER_SEC * 1.0f,
 	    glm::vec3(0.0f, 0.0f, 1.0f)
 	);
+
 	GLint uniTrans = glGetUniformLocation(shaderProgram, "model");
 	glUniformMatrix4fv(uniTrans, 1, GL_FALSE, glm::value_ptr(trans));
 	glUniformMatrix4fv(uniTrans, 1, GL_FALSE, glm::value_ptr(trans));

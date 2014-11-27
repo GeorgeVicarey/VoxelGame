9f7b9f1ffebd3d3fae5fc5f11ac4f2105d4a2462
George Vicarey <G.Vicarey@hotmail.co.uk>
2014-11-27 18:46:30 +0000

    Shader method fileRead now uses a string on the heap instead of on the stack to fix a memory leak.

--- src/shader.cpp
+++ src/shader.cpp
@@ -15,23 +15,21 @@
 const GLchar* Shader::fileRead(std::string filename) {
 
 	//Open file
-	std::string shaderString;
+	std::string * shaderString = new string();
 	std::ifstream sourceFile(filename.c_str());
 
 	//Source file loaded
 	if (sourceFile) {
 		//Get shader source
-		shaderString.assign((std::istreambuf_iterator<char>(sourceFile)),
+		shaderString->assign((std::istreambuf_iterator<char>(sourceFile)),
 				std::istreambuf_iterator<char>());
 
-		shaderString.insert(0, "#version 150 core\n");
+		shaderString->insert(0, "#version 150 core\n");
+
 		// NULL TERMINATE THE FUCKING STRING!!!
-		shaderString.append("\0");
+		shaderString->append("\0");
 
-		//Set shader source
-		const GLchar* shaderSource = shaderString.c_str();
-
-		return shaderSource;
+		return shaderString->c_str();;
 	}
 
 	return NULL;
